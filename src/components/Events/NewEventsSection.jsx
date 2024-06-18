a1d27a651976fc7028d3d6c40442dff272c82796
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-18 14:38:31 -0400

    Export fetchEvents to http.js

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -10,24 +10,6 @@ export default function NewEventsSection() {
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        async function fetchEvents() {
-            setIsLoading(true);
-            const response = await fetch("http://localhost:3000/events");
-
-            if (!response.ok) {
-                const error = new Error(
-                    "An error occurred while fetching the events"
-                );
-                error.code = response.status;
-                error.info = await response.json();
-                throw error;
-            }
-
-            const { events } = await response.json();
-
-            return events;
-        }
-
         fetchEvents()
             .then((events) => {
                 setData(events);
