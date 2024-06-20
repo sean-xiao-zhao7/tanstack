a4923a544c341bac4148f41f50914cee148aa1f3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-20 16:13:30 -0400

    add fetchSelectableImages

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 
 import ImagePicker from "../ImagePicker.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchSelectableImages } from "../../util/http.js";
 
 export default function EventForm({ inputData, onSubmit, children }) {
     const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ["eventFetchImages"],
+        queryFn: fetchSelectableImages,
+        // enabled: selectedImage !== undefined,
+    });
 
     function handleSelectImage(image) {
         setSelectedImage(image);
@@ -29,15 +37,15 @@ export default function EventForm({ inputData, onSubmit, children }) {
                     defaultValue={inputData?.title ?? ""}
                 />
             </p>
-
-            <div className="control">
-                <ImagePicker
-                    images={[]}
-                    onSelect={handleSelectImage}
-                    selectedImage={selectedImage}
-                />
-            </div>
-
+            {data && (
+                <div className="control">
+                    <ImagePicker
+                        images={data}
+                        onSelect={handleSelectImage}
+                        selectedImage={selectedImage}
+                    />
+                </div>
+            )}
             <p className="control">
                 <label htmlFor="description">Description</label>
                 <textarea
--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,32 +1,39 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useMutation } from "@tanstack/react-query";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 import { createNewEvent } from "../../util/http.js";
 
 export default function NewEvent() {
     const navigate = useNavigate();
 
-    const { data, mutate, isError, error } = useMutation({
+    const { data, mutate, isPending, isError, error } = useMutation({
         mutationFn: createNewEvent,
     });
 
-    function handleSubmit(formData) {}
+    function handleSubmit(formData) {
+        mutate({ event: formData });
+    }
 
     return (
         <Modal onClose={() => navigate("../")}>
             <EventForm onSubmit={handleSubmit}>
-                <>
-                    <Link to="../" className="button-text">
-                        Cancel
-                    </Link>
-                    <button type="submit" className="button">
-                        Create
-                    </button>
-                </>
+                {isPending && <LoadingIndicator />}
+                {!isPending && (
+                    <>
+                        <Link to="../" className="button-text">
+                            Cancel
+                        </Link>
+                        <button type="submit" className="button">
+                            Create
+                        </button>
+                    </>
+                )}
             </EventForm>
+            {isError && <p>{error.message}</p>}
         </Modal>
     );
 }
