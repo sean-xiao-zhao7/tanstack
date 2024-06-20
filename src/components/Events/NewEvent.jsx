77ff82e9d99e762c4845d055990e671a0f3ceb35
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-20 15:20:10 -0400

    Use useMutation

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,32 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { useMutation } from "@tanstack/react-query";
+
+import { createNewEvent } from "../../util/http.js";
 
 export default function NewEvent() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+    const { data, mutate, isError, error } = useMutation({
+        mutationFn: createNewEvent,
+    });
 
-  return (
-    <Modal onClose={() => navigate('../')}>
-      <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
-      </EventForm>
-    </Modal>
-  );
+    function handleSubmit(formData) {}
+
+    return (
+        <Modal onClose={() => navigate("../")}>
+            <EventForm onSubmit={handleSubmit}>
+                <>
+                    <Link to="../" className="button-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="button">
+                        Create
+                    </button>
+                </>
+            </EventForm>
+        </Modal>
+    );
 }
