342a85b9a8bd1d5a2342577d4bddd425de8260dc
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-20 15:14:08 -0400

    Add query disabling

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -9,11 +9,12 @@ import EventItem from "./EventItem";
 
 export default function FindEventSection() {
     const searchElement = useRef();
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState();
 
-    const { data, isPending, isError, error } = useQuery({
+    const { data, isLoading, isError, error } = useQuery({
         queryKey: ["fetchEvents", { searchTerm: searchTerm }],
-        queryFn: () => fetchEvents(searchTerm),
+        queryFn: ({ signal }) => fetchEvents({ signal, searchTerm }),
+        enabled: searchTerm !== undefined,
     });
 
     function handleSubmit(event) {
@@ -23,7 +24,7 @@ export default function FindEventSection() {
 
     let content = <p>Please enter a search term and to find events.</p>;
 
-    if (isPending) {
+    if (isLoading) {
         content = <LoadingIndicator />;
     } else if (isError) {
         content = (
