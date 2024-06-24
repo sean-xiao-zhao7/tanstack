cdc0176dc80b09b1fd207deebef558746e59d1c1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-24 13:32:46 -0400

    Add useQuery in EventDetails

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,35 +1,47 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { fetchEvent } from "../../util/http.js";
 
 export default function EventDetails() {
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
-    </>
-  );
+    const { id } = useParams();
+
+    const { data, isLoading, isError, error } = useQuery({
+        queryKey: ["fetchSingleEvent", { id: id }],
+        queryFn: ({ signal }) => fetchEvent({ signal, id }),
+        enabled: id !== undefined,
+    });
+
+    return (
+        <>
+            <Outlet />
+            <Header>
+                <Link to="/events" className="nav-item">
+                    View all Events
+                </Link>
+            </Header>
+            <article id="event-details">
+                <header>
+                    <h1>EVENT TITLE</h1>
+                    <nav>
+                        <button>Delete</button>
+                        <Link to="edit">Edit</Link>
+                    </nav>
+                </header>
+                <div id="event-details-content">
+                    <img src="" alt="" />
+                    <div id="event-details-info">
+                        <div>
+                            <p id="event-details-location">EVENT LOCATION</p>
+                            <time dateTime={`Todo-DateT$Todo-Time`}>
+                                DATE @ TIME
+                            </time>
+                        </div>
+                        <p id="event-details-description">EVENT DESCRIPTION</p>
+                    </div>
+                </div>
+            </article>
+        </>
+    );
 }
