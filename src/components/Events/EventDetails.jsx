78ec99f70a4ab7ff7aba39b3029c6fbd2d3645d8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-24 13:37:56 -0400

    Add loading indicator to eventdetails

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,7 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 import Header from "../Header.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import { fetchEvent } from "../../util/http.js";
 
 export default function EventDetails() {
@@ -22,25 +24,38 @@ export default function EventDetails() {
                 </Link>
             </Header>
             <article id="event-details">
-                <header>
-                    <h1>EVENT TITLE</h1>
-                    <nav>
-                        <button>Delete</button>
-                        <Link to="edit">Edit</Link>
-                    </nav>
-                </header>
-                <div id="event-details-content">
-                    <img src="" alt="" />
-                    <div id="event-details-info">
-                        <div>
-                            <p id="event-details-location">EVENT LOCATION</p>
-                            <time dateTime={`Todo-DateT$Todo-Time`}>
-                                DATE @ TIME
-                            </time>
+                {isLoading && (
+                    <header>
+                        <LoadingIndicator />
+                    </header>
+                )}
+                {!isLoading && (
+                    <>
+                        <header>
+                            <h1>EVENT TITLE</h1>
+                            <nav>
+                                <button>Delete</button>
+                                <Link to="edit">Edit</Link>
+                            </nav>
+                        </header>
+                        <div id="event-details-content">
+                            <img src="" alt="" />
+                            <div id="event-details-info">
+                                <div>
+                                    <p id="event-details-location">
+                                        EVENT LOCATION
+                                    </p>
+                                    <time dateTime={`Todo-DateT$Todo-Time`}>
+                                        DATE @ TIME
+                                    </time>
+                                </div>
+                                <p id="event-details-description">
+                                    EVENT DESCRIPTION
+                                </p>
+                            </div>
                         </div>
-                        <p id="event-details-description">EVENT DESCRIPTION</p>
-                    </div>
-                </div>
+                    </>
+                )}
             </article>
         </>
     );
