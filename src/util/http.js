0ffa4de32da34bda9a5abad2be4e624c2dde99db
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-06-26 16:50:28 -0400

    Add updateEvent to http.js

--- src/util/http.js
+++ src/util/http.js
@@ -93,3 +93,23 @@ export async function deleteEvent({ signal, id }) {
 
     return response.json();
 }
+
+export async function updateEvent({ signal, id, event }) {
+    const response = await fetch(`http://localhost:3000/events/${id}`, {
+        method: "PUT",
+        body: JSON.stringify({ event }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        signal,
+    });
+
+    if (!response.ok) {
+        const error = new Error("An error occurred while updating the event");
+        error.code = response.status;
+        error.info = await response.json();
+        throw error;
+    }
+
+    return response.json();
+}
