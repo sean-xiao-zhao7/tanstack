0802404a98f514c2ae438817474791813f1e8ddd
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-07-03 14:49:32 -0400

    Add error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EventDetails from "./components/Events/EventDetails.jsx";
-import NewEvent from "./components/Events/NewEvent.jsx";
-import EditEvent from "./components/Events/EditEvent.jsx";
 import { QueryClientProvider } from "@tanstack/react-query";
+
 import { queryClient } from "./util/http.js";
+
 import AllEventsPage from "./components/Pages/AllEventsPage.jsx";
 import MainLayout from "./components/Layout/MainLayout.jsx";
 import HomePage from "./components/Pages/HomePage.jsx";
+import ErrorPage from "./components/Pages/ErrorPage.jsx";
+
+import EventDetails from "./components/Events/EventDetails.jsx";
+import NewEvent from "./components/Events/NewEvent.jsx";
+import EditEvent from "./components/Events/EditEvent.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
